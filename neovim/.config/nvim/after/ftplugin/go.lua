13447ba5e80529b8f5c66fe7e7d217fbbede2452
Dakota Walsh <kota@nilsu.org>
2022-05-25 17:14:21 +1200

    tweak neovim ftplugins

--- neovim/.config/nvim/after/ftplugin/go.lua
+++ neovim/.config/nvim/after/ftplugin/go.lua
@@ -5,6 +5,7 @@ local noremap = { noremap = true }
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
+vim.o.textwidth = 80
 vim.o.expandtab = false
 
 -- Run the project
--- neovim/.config/nvim/after/ftplugin/hare.lua
+++ neovim/.config/nvim/after/ftplugin/hare.lua
@@ -1,6 +1,12 @@
 local map = vim.api.nvim_buf_set_keymap
 local noremap = { noremap = true }
 
+vim.o.softtabstop = 8
+vim.o.shiftwidth = 8
+vim.o.tabstop = 8
+vim.o.textwidth = 80
+vim.o.expandtab = false
+
 -- run the project
 map(0, "n", "<leader>r", "<cmd>!hare run %<cr>", noremap)
 
