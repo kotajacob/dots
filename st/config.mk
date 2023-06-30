7693a6b760998ca666026fd758885ea17f352c0c
Dakota Walsh <kota@nilsu.org>
2023-06-30 21:32:20 +1200

    yepun initial setup

--- st/config.mk
+++ st/config.mk
@@ -4,7 +4,7 @@ VERSION = 0.9
 # Customize below to fit your system
 
 # paths
-PREFIX = $(HOME)/.local/stow/st
+PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
 X11INC = /usr/X11R6/include
@@ -24,13 +24,3 @@ LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
 STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
 STLDFLAGS = $(LIBS) $(LDFLAGS)
-
-# OpenBSD:
-#CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
-#LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \
-#       `$(PKG_CONFIG) --libs fontconfig` \
-#       `$(PKG_CONFIG) --libs freetype2`
-#MANPREFIX = ${PREFIX}/man
-
-# compiler and linker
-# CC = c99
